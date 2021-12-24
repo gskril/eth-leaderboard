777e9b60420c89667e28bfd422195dfe4f8c8185
Greg <gskril@gmail.com>
2021-12-24 01:40:11 -0500

    Update meta tags and favicon

--- public/app.js
+++ public/app.js
@@ -1,12 +1,1 @@
-function copyData() {
-	var copyText = document.getElementById("json");
-	copyText.select();
-  	copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-	navigator.clipboard.writeText(copyText.value)
-		.then(function() {
-			alert("Copied the JSON data to clipboard");
-		}, function() {
-			console.log('Failed')
-		});
-}
