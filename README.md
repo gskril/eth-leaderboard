6c1132aab143f81a93c45d529f81fb6484957b0e
Greg <gskril@gmail.com>
2022-01-06 21:50:23 -0500

    Add search bar

---
+++ README.md
@@ -0,0 +1,12 @@
+# ETH Leaderboard
+
+The most followed accounts with .eth names on Twitter
+
+Build with Express.js and EJS. Deployed on [Railway](https://railway.app/).
+
+## Ideas
+- [x] Search bar
+- [ ] Improved Twitter monitor
+- [ ] Better way to deal with ENS avatars
+- [ ] Filtering (ENS avatar, Twitter verified)
+- [ ] Daily/weekly snapshots of the leaderboard
--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
+const search = document.getElementById('search')
+const profiles = document.querySelectorAll('.profiles tbody > tr')
 
+search.addEventListener('keyup', () => {
+	profiles.forEach(profile => {
+		const text = profile.textContent.toLowerCase()
+		if (!text.includes(search.value.toLowerCase())) {
+			profile.classList.add('hide')
+		} else {
+			profile.classList = ''
+		}
+	})
+
+	// check if there are any profiles left
+	const visibleProfiles = document.querySelectorAll('.profiles tbody > tr:not(.hide)')
+	if (visibleProfiles.length === 0) {
+		document.querySelector('.no-results').style.display = 'block'
+	} else {
+		document.querySelector('.no-results').style.display = 'none'
+	}
+})
