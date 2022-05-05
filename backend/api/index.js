fe1b1cd56a2d189177897dcb5dfae299ab191664
Greg <gskril@gmail.com>
2022-05-05 13:59:08 -0400

    Remove backend files

--- backend/api/index.js
+++
@@ -1,8 +0,0 @@
-export { addFren, addFrens, getAllFrens, updateFren, updateFrens } from "./db";
-export { fetchEnsStats } from "./external";
-export {
-  getTwitterProfileByHandle,
-  refreshDatabase,
-  tweetNewProfile,
-  updateTwitterLocation,
-} from "./twitter";
