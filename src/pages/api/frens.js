6e49a5fbd3774b7915af098d927ce426091e7d41
Greg <gskril@gmail.com>
2022-06-28 00:33:54 -0400

    Include time in API response

--- src/pages/api/frens.js
+++ src/pages/api/frens.js
@@ -2,15 +2,17 @@ import { fetchInitialData } from "../../staticapi";
 
 export default async (req, res) => {
   if (req.method === "GET") {
+    const startTime = Date.now();
     console.log("FETCHING DATA FROM API", req.query);
     let { q, count, skip, verified, location } = req.query;
     // Limit to 100 results per page
     count ? (count > 100 ? (count = 100) : count) : (count = 100)
     skip |= 0;
-    location ? location : null
 
     const data = await fetchInitialData(q, count, skip, verified, location);
+    const endTime = Date.now();
+    const diff = endTime - startTime;
     res.status(200);
-    res.json(data);
+    res.json({ ...data, response_time: diff });
   }
 };
