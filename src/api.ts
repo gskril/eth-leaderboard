0c0339deb3f4b74c06a04798f09e1829f2a0a705
Greg <gskril@gmail.com>
2023-01-18 02:32:58 +0200

    Remove unused code

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,8 @@
 import useSWR from 'swr';
 
-import { server } from './config';
-
 const frensPath = '/api/frens?';
-const metaPath = '/api/meta';
 
 const fetcher = (args: RequestInfo) => fetch(args).then((res) => res.json());
-
-export const initialFetchMeta = () =>
-  fetch(server + metaPath).then((res) => res.json());
-export const initialFetchFrens = () =>
-  fetch(server + frensPath).then((res) => res.json());
 
 interface UseFrens {
   searchInput?: string;
--- src/config/index.ts
+++
@@ -1,5 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production';
-
-export const server = dev
-  ? 'http://localhost:3000'
-  : `https://${process.env.RAILWAY_STATIC_URL}`;
