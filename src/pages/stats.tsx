23785c28580c273f6b31080d0d479725120f58e8
Greg <gskril@gmail.com>
2023-01-22 22:37:13 +0200

    Add location data

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,5 +1,6 @@
+import { ResponsiveBar } from '@nivo/bar';
 import { ResponsiveLine } from '@nivo/line';
-import { useEffect, useState } from 'react';
+import { useWindowSize } from 'usehooks-ts';
 import Head from 'next/head';
 
 import { fetchInitialMetadata } from '../staticapi';
@@ -17,17 +18,51 @@ type LineData = {
   }[];
 }[];
 
+type BarData = {
+  region: string;
+  count: number;
+  color: string;
+}[];
+
 type EthCount = {
   date: Date;
   count: number;
 };
 
 type StatsProps = {
-  data: LineData;
+  barData: BarData;
+  lineData: LineData;
   frensMeta: Metadata;
 };
 
-export default function Stats({ data, frensMeta }: StatsProps) {
+const nivoTheme = {
+  background: 'transparent',
+  textColor: 'var(--text-color)',
+  axis: {
+    ticks: {
+      line: {
+        stroke: 'var(--text-color-lightest)',
+      },
+      text: {
+        fontSize: 11,
+        fill: 'var(--text-color-light)',
+      },
+    },
+  },
+  grid: {
+    line: {
+      stroke: 'var(--text-color-lightest)',
+    },
+  },
+  tooltip: {
+    container: {
+      background: 'var(--background-accent-light)',
+      border: '1px solid var(--text-color-lighter)',
+    },
+  },
+};
+
+export default function Stats({ barData, lineData, frensMeta }: StatsProps) {
   return (
     <Layout>
       <Head>
@@ -63,7 +98,18 @@ export default function Stats({ data, frensMeta }: StatsProps) {
         </div>
 
         <div className="line">
-          <MyResponsiveLine data={data} />
+          <MyResponsiveLine data={lineData} />
+        </div>
+
+        <div className="container--small">
+          <p>
+            This bar graph represents the number of .eth profiles in popular
+            regions based on their Twitter location.
+          </p>
+        </div>
+
+        <div className="line">
+          <MyResponsiveBar data={barData} />
         </div>
       </div>
 
@@ -72,7 +118,7 @@ export default function Stats({ data, frensMeta }: StatsProps) {
           width: 100%;
           max-width: 55rem;
           margin: 0 auto;
-          padding-bottom: 2rem;
+          padding: 0 1rem;
         }
 
         .container--small {
@@ -88,6 +134,7 @@ export default function Stats({ data, frensMeta }: StatsProps) {
         }
 
         .line {
+          margin-bottom: 4rem;
           width: 100%;
           height: min(30rem, 100vh);
         }
@@ -96,67 +143,80 @@ export default function Stats({ data, frensMeta }: StatsProps) {
   );
 }
 
-const MyResponsiveLine = ({ data }: { data: LineData }) => (
-  <ResponsiveLine
+const MyResponsiveLine = ({ data }: { data: LineData }) => {
+  const { width } = useWindowSize();
+  const isMobile = width && width < 768;
+
+  return (
+    <ResponsiveLine
+      data={data}
+      margin={{
+        top: 5,
+        right: 12,
+        bottom: isMobile ? 65 : 50,
+        left: isMobile ? 35 : 65,
+      }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 85_000,
+        max: 'auto',
+        stacked: true,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      axisBottom={{
+        tickRotation: isMobile ? -90 : 0,
+        legend: 'Date',
+        legendOffset: isMobile ? 60 : 45,
+        tickSize: 10,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        tickRotation: 0,
+        legend: isMobile ? null : '.eth Profiles',
+        legendOffset: -60,
+        legendPosition: 'middle',
+        format: (v) =>
+          `${new Intl.NumberFormat('en-US', {
+            notation: 'compact',
+          }).format(v)}`,
+      }}
+      theme={nivoTheme}
+      colors={{ datum: 'color' }}
+      pointColor={{ theme: 'background' }}
+      pointLabelYOffset={-200}
+      useMesh={true}
+    />
+  );
+};
+
+const MyResponsiveBar = ({ data }: { data: BarData }) => (
+  <ResponsiveBar
     data={data}
-    margin={{ top: 5, right: 15, bottom: 50, left: 65 }}
-    xScale={{ type: 'point' }}
-    yScale={{
-      type: 'linear',
-      min: 'auto',
-      max: 'auto',
-      stacked: true,
-      reverse: false,
+    keys={['count']}
+    layout="vertical"
+    indexBy="region"
+    margin={{ top: 0, right: 0, bottom: 20, left: 0 }}
+    padding={0.3}
+    colors={{ datum: 'data.color' }}
+    labelTextColor={{
+      from: 'color',
+      modifiers: [['darker', 1.6]],
     }}
-    yFormat=" >-.2f"
+    axisLeft={null}
     axisBottom={{
+      tickSize: 0,
+      tickPadding: 10,
+      tickValues: 5,
       tickRotation: 0,
-      legend: 'Date',
-      legendOffset: 45,
-      legendPosition: 'middle',
     }}
-    axisLeft={{
-      tickRotation: 0,
-      legend: '.eth Profiles',
-      legendOffset: -60,
-      legendPosition: 'middle',
-      format: (v) =>
-        `${new Intl.NumberFormat('en-US', {
-          notation: 'compact',
-        }).format(v)}`,
+    theme={nivoTheme}
+    role="application"
+    ariaLabel=".eth Twitter profiles per region"
+    barAriaLabel={function (e) {
+      return e.id + ': ' + e.formattedValue + ' in region: ' + e.indexValue;
     }}
-    theme={{
-      background: 'transparent',
-      textColor: 'var(--text-color)',
-      axis: {
-        ticks: {
-          line: {
-            stroke: 'var(--text-color-lightest)',
-          },
-          text: {
-            fontSize: 11,
-            fill: 'var(--text-color-light)',
-          },
-        },
-      },
-      grid: {
-        line: {
-          stroke: 'var(--text-color-lightest)',
-        },
-      },
-      tooltip: {
-        container: {
-          background: 'var(--background-accent-light)',
-          border: '1px solid var(--text-color-lighter)',
-        },
-      },
-    }}
-    colors={{
-      datum: 'color',
-    }}
-    pointColor={{ theme: 'background' }}
-    pointLabelYOffset={-200}
-    useMesh={true}
   />
 );
 
@@ -167,7 +227,7 @@ export async function getStaticProps() {
   // only count every 14th entry
   const entriesSpaced = entries.filter((_, i) => i % 14 === 0);
 
-  const data: LineData = [
+  const lineData: LineData = [
     {
       id: '.eth Profiles',
       color: 'var(--text-color-accent)',
@@ -181,11 +241,62 @@ export async function getStaticProps() {
     },
   ];
 
+  const newYork = await db['eth'].count({
+    or: [
+      { 'location ilike': 'ny' },
+      { 'location ilike': '%nyc%' },
+      { 'location ilike': '%, ny%' },
+      { 'location ilike': '%new york%' },
+      { 'location ilike': '%brooklyn%' },
+    ],
+  });
+
+  const california = await db['eth'].count({
+    or: [
+      { 'location ilike': 'ca' },
+      { 'location ilike': '%, ca%' },
+      { 'location ilike': '%california%' },
+      { 'location ilike': '%los angeles%' },
+      { 'location ilike': '%san francisco%' },
+      { 'location ilike': '%bay area%' },
+      { 'location ilike': '%san diego%' },
+    ],
+  });
+
+  const texas = await db['eth'].count({
+    or: [
+      { 'location ilike': 'tx' },
+      { 'location ilike': '%, tx%' },
+      { 'location ilike': '%texas%' },
+      { 'location ilike': '%austin%' },
+      { 'location ilike': '%dallas%' },
+    ],
+  });
+
+  const barData: BarData = [
+    {
+      region: 'New York',
+      count: newYork,
+      color: 'var(--text-color-accent)',
+    },
+    {
+      region: 'California',
+      count: california,
+      color: 'var(--text-color-accent)',
+    },
+    {
+      region: 'Texas',
+      count: texas,
+      color: 'var(--text-color-accent)',
+    },
+  ];
+
   const frensMeta = await fetchInitialMetadata();
 
   return {
     props: {
-      data,
+      lineData,
+      barData,
       frensMeta,
     },
     revalidate: 60 * 60, // 1 hour
